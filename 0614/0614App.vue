ea2196c310703a8edccaa5b31a631d748bc94cb0
Park Chaerhin <127855706+park-chaerhin@users.noreply.github.com>
2023-06-20 15:54:01 +0900

    Vue
    Vue

---
+++ 0614/0614App.vue
@@ -0,0 +1,15 @@
+<template>
+    <div id="app">
+        <div id="header"></div>
+        <div id="main"></div>
+        <div id="footer"></div>
+    </div>
+</template>
+<style>
+ html, body{height: 100%;}
+ body{margin: 0; padding: 0;}
+ #app{height: 100%;}
+ #header{background: rgb(255,170,170); height: 100px;}
+ #main{background: rgb(165,250,165); height: calc(100% - 150px);}
+ #footer{background: rgb(156,156,255); height: 50px;}
+</style>
