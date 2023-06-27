87c59b4ab27e8c492dc869c04e2765219b7c5eb3
Park Chaerhin <127855706+park-chaerhin@users.noreply.github.com>
2023-06-27 15:28:09 +0900

    vue2_vs_vue3
    vue2_vs_vue3

---
+++ 0627/Vue3/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <!--Option API-->
+  <div id="date">
+    <h1>Option API</h1>
+    {{ date }}
+  </div>
+
+  <!--Composition API-->
+  <div id="date2">
+    <h1>Composition API</h1>
+    {{ date2 }}
+  </div>
+</template>
+
+<script>
+  export default {
+    //Option API
+    data(){
+      return {
+        date: Date().toString(),
+      }
+    },
+
+    //Composition API
+    setup(){
+      const date2 = Date().toString()
+      return{
+        date2,
+      }
+    }
+  }
+</script>
+
---
+++ 0627/Vue3/opt_vs_comp.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <div>
+      <h1>Option API</h1>
+      <div v-text="vhtml"></div>
+      <div v-html="vhtml2"></div>
+    </div>
+
+    <div>
+      <h1>Composition API</h1>
+      <div v-text="vhtml"></div>
+      <div v-html="vhtml2"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    //Option API
+    data(){
+      return {
+        vhtml: '<h2>Options API TEXT</h2>'
+      }
+    },
+
+    //Composition API
+    setup(){
+      const vhtml2 = '<h2>Options API TEXT</h2>'
+      return{
+        // vhtml2 : vhtml2
+        vhtml2,
+      }
+    }
+  }
+</script>
+
